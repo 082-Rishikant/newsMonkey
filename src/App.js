59be46cc3268c70e3a6f2b1e791c898be2302a8a
Rishikant Patel <38522693+082-Rishikant@users.noreply.github.com>
2021-12-11 20:47:45 +0530

    Lecture-22 Project-2 newapp SetUp+class based component Intro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component
+{
+  name="Rishi"
+  render(){
+    return(
+      <>
+        <h2>Hello {this.name}, This is my first Class based Component</h2>
+      </>
+    );
+  }
 }
-
-export default App;
