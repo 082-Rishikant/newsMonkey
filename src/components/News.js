cf61be5c2a54caf5e0274d41da9914f47d916768
Rishikant Patel <38522693+082-Rishikant@users.noreply.github.com>
2021-12-11 22:36:30 +0530

    Lecture-24 Fetching API key from News API

--- src/components/News.js
+++ src/components/News.js
@@ -4,13 +4,20 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div className='container'>
-        this is News component
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2>Top headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+          <NewsItem title={'title'} description={'description'}/>
+          </div>
+          <div className="col-md-4">
+          <NewsItem title={'title'} description={'description'}/>
+          </div>
+          <div className="col-md-4">
+          <NewsItem title={'title'} description={'description'}/>
+          </div>
+        </div>
+        
       </div>
     )
   }
